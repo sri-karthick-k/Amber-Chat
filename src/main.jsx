5c1b45a304649cbd3af114ce231bdc726dbb306c
sri-karthick-k <srikarthickk.mca22@rvce.edu.in>
2023-12-31 12:50:45 +0530

    add chat route

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
+// import Router from './routes.js'
+import ChatWindow from './components/chat/ChatWindow.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes >
+        <Route path="/" exact element={<App />} />
+        <Route path="/chat" element={<ChatWindow />} /> 
+
+      </Routes> 
+    </Router>
+    {/* <BrowserRouter>
+      <Router />
+    </BrowserRouter> */}
   </React.StrictMode>,
 )
