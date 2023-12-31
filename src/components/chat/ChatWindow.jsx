f679b73cf73753dcf96b4068f321ad5dde3e614e
sri-karthick-k <srikarthickk.mca22@rvce.edu.in>
2023-12-31 12:51:37 +0530

    init Chat window

--- src/components/chat/ChatWindow.jsx
+++ src/components/chat/ChatWindow.jsx
@@ -1,8 +1,58 @@
+import { useState } from "react"
+
 const ChatWindow = () => {
+
+    const [message, setMessage] = useState("");
+    const [localSaveMsg, setLocalSaveMsg] = useState([]);
+
+    function onMessageChange (event) {
+        setMessage(event.target.value)
+
+    }
+    
+    function sendMessage(event) {
+        if(event.ctrlKey && event.key === "Enter"){
+            alert(message)
+            setLocalSaveMsg(localSaveMsg.push(message))
+        }
+    }
     return (
-        <div>
+        <div className="chat-window full-width">
             <h1>Chat Window</h1>
-            {/* <h4>{props}</h4> */}
+
+            <div className="profile-nav">
+                <p className="username-font">Username here</p>
+            </div>
+
+            <div className="chat-container">
+                <textarea 
+                    id="userInput" 
+                    placeholder="Type your message..."
+                    value={message}
+                    onChange={onMessageChange}
+                    onKeyDown={sendMessage}
+                />
+                <div className="tool-tip">
+                    Press ctrl + enter to send the message
+                </div>
+            </div>
+
+            <div className="prev-chat">
+                <p>You typed: {message}</p>
+            </div>
+
+            <ul>
+                {localSaveMsg.map((msg, index) => (
+                    <li
+                        key={index}  
+                    >
+                        {msg}
+                    </li>
+                ))}
+            </ul>
+
+
+
         </div>
     )
 }
